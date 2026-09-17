61157e7388012f4ee239cebb6c2dc4bea4ef45ae
niulaima <niulaima@hotmail.com>
2026-09-17 15:26:23 +0630

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || '81960df1-9f0f-4727-bcbd-014cd6cb09f9';
+const UUID = process.env.UUID || 'a2e664bb-4a88-42c2-bc37-a98fd2a4bb77';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
